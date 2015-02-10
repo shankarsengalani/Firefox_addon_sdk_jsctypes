5591ab51113fb8e63e67a9ff4e00569098bf0edc
shankar2105 <shankar21mail@gmail.com>
2015-02-10 15:23:42 +0530

    Added C++ code.

---
+++ c++/ctypes.h
@@ -0,0 +1,6 @@
+#ifndef _ctypes_h
+#define _ctypes_h
+
+extern "C" __declspec( dllexport ) int add(int a, int b);
+
+#endif
---
+++ c++/jsctype_c.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include "ctypes.h"
+
+int add(int a, int b) 
+{
+	return(a + b);
+}
+
+
